9dc13b22ae0ddf88e6afa7aafb471390311cc2e0
Pranay Tamada <pranay.tamada@gmail.com>
2026-07-01 23:12:26 +0530

    Intial Setup

---
+++ server/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Unisphere sever is running !")
+});
+
+export default app;
